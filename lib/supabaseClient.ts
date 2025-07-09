8803dcb428bfcf0f59b50c41d6ac5b1aa36f5349
Ram-start2020 <63640172+Ram-start2020@users.noreply.github.com>
2025-07-09 23:16:47 +0300

    Update supabaseClient.ts
    add supabase_URL and supabase anon key

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -8,7 +8,7 @@ import { SUPABASE_URL, SUPABASE_ANON_KEY } from '../config';
 // The app expects a client object to exist. We initialize it here,
 // and the logic in App.tsx will prevent its use if the config is invalid,
 // showing a friendly error message instead of crashing.
-if (SUPABASE_URL === 'YOUR_SUPABASE_URL' || SUPABASE_ANON_KEY === 'YOUR_SUPABASE_ANON_KEY') {
+if (SUPABASE_URL === 'https://sodopikctzypkmczimfe.supabase.co' || SUPABASE_ANON_KEY === 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvZG9waWtjdHp5cGttY3ppbWZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3Mjk2NTksImV4cCI6MjA2NzMwNTY1OX0.6Yus-NmgQ8xSC0odS91g5GudoXtC64oUOMICYDg09Kw') {
   console.error("Supabase configuration is missing. Please add your URL and Anon Key to config.ts");
 }
 
